e3731071d733c285812d6fd49c472995f6094bda
ddmills <mills.dalton@gmail.com>
2017-12-09 16:48:44 -0600

    :frog: add scaffolding

---
+++ test/StringCalculator.spec.js
@@ -0,0 +1,9 @@
+import {expect} from 'chai';
+import sinon from 'sinon';
+import StringCalculator from '../src/StringCalculator';
+
+describe('StringCalculator', () => {
+  it('should do something...', () => {
+    expect('testing driven development').to.include('drive');
+  });
+});
