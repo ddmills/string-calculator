22d6c6d91bd9070306210fa65e9f35784e6b1d47
Theo Kalfas <tkalfas@sourceallies.com>
2018-06-11 17:19:18 -0500

    Added help for Chai and Mocha documentation

--- test/StringCalculator.spec.js
+++ test/StringCalculator.spec.js
@@ -1,6 +1,11 @@
 import {expect} from 'chai';
 import StringCalculator from '../src/StringCalculator';
 
+// Refer to the Mocha documentation for the structure of the tests.
+//    https://mochajs.org/#getting-started
+// Refer to the Chai documentation for assertions using `expect`.
+//    http://www.chaijs.com/api/bdd/
+
 describe('StringCalculator', () => {
   it('should fail', () => {
     expect.fail(0, 1, "This test always fails");
